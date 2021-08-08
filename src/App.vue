7d773dcb449988794053a9adac3a0971cb8af557
d0kur0 <dev.dokuro@gmail.com>
2021-08-09 02:47:09 +0300

    Commit all changes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <VoiceRecord />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import VoiceRecord from './components/VoiceRecord.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VoiceRecord
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/VoiceRecord.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="voice-record">
+    Voice record component
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VoiceRecord',
+  props: {}
+})
+</script>
+
+<style scoped>
+.voice-record {
+  height: 550px;
+}
+</style>
